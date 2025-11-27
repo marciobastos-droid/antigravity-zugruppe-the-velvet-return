6de5b556a0edff0b4d6971904243094e4c3a02af
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2025-11-27 20:08:46 +0000

    File changes

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -30,6 +30,7 @@ import EmailHub from "../components/email/EmailHub";
 import CreditSimulator from "../components/tools/CreditSimulator";
 import ReportsExporter from "../components/tools/ReportsExporter";
 import WhatsAppIntegration from "../components/tools/WhatsAppIntegration";
+import LeadManagementHub from "../components/leads/LeadManagementHub";
 
 export default function Tools() {
   const [activeTab, setActiveTab] = useState("importLeads");
