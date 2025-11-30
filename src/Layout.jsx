a0249c9217984392a7d18937dc541fb6005f912c
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2025-11-30 17:31:51 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -37,6 +37,7 @@ export default function Layout({ children, currentPageName }) {
     
     { name: "Tools", path: createPageUrl("Tools"), icon: Wrench, id: "nav-tools", visibility: ['admin', 'gestor'] },
     { name: "Relatórios", path: createPageUrl("Reports"), icon: FileBarChart, id: "nav-reports", visibility: ['admin', 'gestor', 'agente'] },
+    { name: "Equipa", path: createPageUrl("TeamManagement"), icon: Users, id: "nav-team", visibility: ['admin', 'gestor'] },
     { name: "Utilizadores", path: createPageUrl("UserManagement"), icon: Users, id: "nav-users", visibility: ['admin'] },
   ];
 
