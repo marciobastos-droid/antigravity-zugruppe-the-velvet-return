30f1ce6afac3ec1964809ed8dc1a46b324035931
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2025-12-05 14:23:28 +0000

    File changes

--- src/pages/MyListings.jsx
+++ src/pages/MyListings.jsx
@@ -23,6 +23,7 @@ import BulkPhotoAssign from "../components/listings/BulkPhotoAssign";
 import CreateDevelopmentFromProperties from "@/components/developments/CreateDevelopmentFromProperties";
 import AdvancedFilters, { FILTER_TYPES } from "@/components/filters/AdvancedFilters";
 import { useAdvancedFilters } from "@/components/filters/useAdvancedFilters";
+import { useUndoAction } from "@/components/common/useUndoAction";
 
 // Memoized Property Card component for better performance
 const PropertyCard = memo(function PropertyCard({ 
