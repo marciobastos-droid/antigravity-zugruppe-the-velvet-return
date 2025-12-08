33b00bb9da3f20d36a9bc429aad80f90dd1c7c96
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2025-12-08 20:31:34 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
   },
   {
     image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=400&h=300&fit=crop",
-    title: "WebPage",
+    title: "WebSite",
     path: "ZuGruppe"
   },
   {
