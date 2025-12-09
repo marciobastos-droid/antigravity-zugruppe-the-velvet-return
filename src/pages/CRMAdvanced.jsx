5f48bd9a4cb38ec5535348180791ec6ae5c6ae3a
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2025-12-09 15:08:59 +0000

    File changes

--- src/pages/CRMAdvanced.jsx
+++ src/pages/CRMAdvanced.jsx
@@ -11,11 +11,13 @@ import OpportunitiesContent from "../components/crm/OpportunitiesContent";
 export default function CRMAdvanced() {
   // Auth check - redirect to login if not authenticated
   React.useEffect(() => {
-    base44.auth.isAuthenticated().then(isAuth => {
-      if (!isAuth) {
-        base44.auth.redirectToLogin(window.location.pathname + window.location.search);
-      }
-    });
+    if (typeof base44 !== 'undefined') {
+      base44.auth.isAuthenticated().then(isAuth => {
+        if (!isAuth) {
+          base44.auth.redirectToLogin(window.location.pathname + window.location.search);
+        }
+      });
+    }
   }, []);
 
   // Read tab from URL params
