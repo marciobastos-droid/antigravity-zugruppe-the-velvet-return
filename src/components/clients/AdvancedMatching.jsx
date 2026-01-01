f62c4eb23b7f14274c02ae5e4a24433019f6a3b9
Márcio J. A. Bastos <marcio.bastos@zugruppe.com>
2026-01-01 20:37:44 +0000

    File changes

--- src/components/clients/AdvancedMatching.jsx
+++ src/components/clients/AdvancedMatching.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
